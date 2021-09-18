de96dd99e76e17b096054307a219997f74e4d8f3
chu-young <ttheko101@gmail.com>
2021-09-18 11:05:19 +0200

    added home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 //All pages imported from Pages folder
 
+import Home from "./Pages/Home.js";
 import Navbar from "./Pages/Navbar.js";
 
 function App() {
   return (
+<main>
   <Navbar/>
+  <Home/>
+</main>
   );
 }
 
