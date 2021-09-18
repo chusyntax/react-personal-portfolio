487091837b4cb5c0c6291f6445d85d7bd4c890c3
chu-young <ttheko101@gmail.com>
2021-09-18 11:11:51 +0200

    added assets folder for about page

---
+++ src/assets/icons/Icons.js
@@ -0,0 +1,28 @@
+const Icons = {
+    vscode : require('./vs code.png').default,
+    bootstrap : require('./bootstrap.png').default,
+    
+    css3 : require('./css3.png').default,
+    figma : require('./figma.png').default,
+    
+    html5 : require('./html5.png').default,
+    jquery : require('./jquery.png').default,
+    
+    js : require('./js.png').default,
+    materialize : require('./materialize.png').default,
+
+    react : require('./react.png').default,
+    typescript : require('./typescript.png').default,
+
+    npm : require('./npm.png').default,
+    sass : require('./sass.png').default,
+    
+    git : require('./git.png').default,
+    github : require('./github.png').default,
+
+    wordpress : require('./wordpress.png').default,
+    webpack : require('./webpack.png').default,
+    
+    }
+    
+    export default Icons
