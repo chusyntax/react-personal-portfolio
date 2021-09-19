6a103c23b3984b670e2e9269d43ef1275fceab1e
chu-young <ttheko101@gmail.com>
2021-09-19 14:30:29 +0200

    added assets for cards

---
+++ src/assets/card-images/Images.js
@@ -0,0 +1,27 @@
+const Images = {
+card1 : require('./card 1.png').default,
+card1Mobile : require('./card 1 mobile.png').default,
+
+card2 : require('./card 2.jpg').default,
+card2Mobile : require('./card 2 mobile.png').default,
+
+card3 : require('./card 3.png').default,
+card3Mobile : require('./card 3 mobile.png').default,
+
+card4 : require('./card 4.png').default,
+card4Mobile : require('./card 4 mobile.png').default,
+
+card5 : require('./card 5.png').default,
+card5Mobile : require('./card 5 mobile.png').default,
+
+card6 : require('./card 6.png').default,
+card6Mobile : require('./card 6 mobile.png').default,
+
+card7 : require('./card 7.png').default,
+card7Mobile : require('./card 7 mobile.png').default,
+
+card8 : require('./card 8.png').default,
+card8Mobile : require('./card 8 mobile.png').default,
+}
+
+export default Images
