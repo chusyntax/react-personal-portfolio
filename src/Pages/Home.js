577abc2a96c88437b5b09108cb7995c98d822194
chu-young <ttheko101@gmail.com>
2021-09-19 13:26:11 +0200

    added inline styles to home page intro section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,9 +7,9 @@ function Home() {
 <div className={styles.home}>
 
 <div className={styles.intro}>
-    <h1>Hello World,</h1>
-    <h2>I'm Thabo Theko</h2>
-    <h3>A Front End Web Developer</h3>
+    <h1 style={{fontSize:"5rem"}}>Hello World,</h1>
+    <h2 style={{fontSize:"2.5rem"}}>I'm Chu-young (Chu) Theko.</h2>
+    <h3 style={{fontSize:"1.7rem"}}>A Front End Web Developer</h3>
 </div>
 
 <div className={styles.buttons}>
